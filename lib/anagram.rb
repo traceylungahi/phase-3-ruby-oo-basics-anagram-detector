d98e2560779a3deb2636f1d207cf5478fce8fd42
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-08-16 19:40:08 +0300

    commit tree 5df128003ae059f3d723d4d55758012eaba22680

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram 
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word 
+    end   
+
+    def match(array_words)
+        match_thing = []
+        array_words.map do |thing|
+            if @word.chars.sort == thing.chars.sort
+                match_thing << thing
+            end
+        end
+        match_thing
+    end            
+end       
